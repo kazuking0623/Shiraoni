c9662f083696f45b367cdd1eed82a9cde94bfee8
kazuking0623 <13019@hakodate.kosen-ac.jp>
2017-12-15 10:00:55 +0900

    timer

--- game/MyWorld.java
+++ game/MyWorld.java
@@ -13,9 +13,20 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    private int rtime;
+    public void act()
+    {
+        rtime--;
+        showText( "rtime = " + rtime, 300, 50 );
+        if( rtime == 0 ){
+                   showText( "ゲームオーバー", 100, 50 );
+                   Greenfoot.stop();
+        }  
+    }
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        rtime = 1000;
     }
 }
