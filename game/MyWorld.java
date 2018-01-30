e672271cb2c0ea1f601d2676576c868dfd9db6fd
13093 <13093@hakodate.kosen-ac.jp>
2018-01-30 15:45:25 +0900

    hoge

--- game/MyWorld.java
+++ game/MyWorld.java
@@ -23,7 +23,7 @@ public class MyWorld extends World
                    showText( "ゲームオーバー", 100, 50 );
                    Greenfoot.stop();
         }  
-    }
+    } 
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
