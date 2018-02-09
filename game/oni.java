96afd5176fd3e1ec55c4c511c2c9405816dcca62
13093 <13093@hakodate.kosen-ac.jp>
2018-02-09 10:20:46 +0900

    aa

--- game/oni.java
+++ game/oni.java
@@ -12,16 +12,22 @@ public class oni extends Actor
      * Act - do whatever the oni wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    int a;
+    private int a;
     private int count;
+    private int oni;
+
+    public boolean moving(){ return (a==0); } 
+
     public void act() 
     {
+
         hita();
         hits();
         hitw();
         hitd();
-           // getWorld().showText( "count = " + count, 500, 50 );
-           // getWorld().showText( "a = " + a, 700, 50 );
+        //hit();
+        // getWorld().showText( "count = " + count, 500, 50 );
+        // getWorld().showText( "a = " + a, 700, 50 );
         if(a==1){
             count--;
             if(count == 0){
@@ -29,28 +35,23 @@ public class oni extends Actor
             }
         }
         else{
-        for(int i=0;i<5;i++){
-        int Xmin = 0;
-        int Xmax = 10;
-        int X = Xmin + (int)(Math.random()*((Xmax-Xmin)+1));
-        
-        int Ymin = 0;
-        int Ymax = 360;
-        int Y = Ymin + (int)(Math.random()*((Ymax-Ymin)+1));
-        move(X);
-        setRotation(Y);
-        
+            
+            for(int i=0;i<5;i++){
+                int Xmin = 0;
+                int Xmax = 10;
+                int X = Xmin + (int)(Math.random()*((Xmax-Xmin)+1));
+
+                int Ymin = 0;
+                int Ymax = 360;
+                int Y = Ymin + (int)(Math.random()*((Ymax-Ymin)+1));
+                move(X);
+                setRotation(Y);
+
+            }
+        }
+
     }
-       }
-          /*
-       if( actor != null ){
-             World c = new gameover();
-                   Greenfoot.setWorld(c);
-                   Greenfoot.stop();
-        }  
-           */    
-    }
-    
+
     public void hita()
     {
         Actor actor = getOneObjectAtOffset( 0, 0, bulleta.class );
@@ -58,10 +59,11 @@ public class oni extends Actor
             getWorld().removeObject( actor ); // getWorld() は World オブジェクトの getter
             a = 1;
             count = 200;
-            // TARO とぶつかった時の処理を書く
+            // TARO とぶつかった時の処理を書く       
         }  
     }
-        public void hitw()
+
+    public void hitw()
     {
         Actor actor = getOneObjectAtOffset( 0, 0, bulletw.class );
         if( actor != null ){
@@ -71,17 +73,19 @@ public class oni extends Actor
             // TARO とぶつかった時の処理を書く
         }  
     }
-        public void hits()
+
+    public void hits()
     {
         Actor actor = getOneObjectAtOffset( 0, 0, bullets.class );
         if( actor != null ){
             getWorld().removeObject( actor ); // getWorld() は World オブジェクトの getter
-           a = 1;
-           count = 200;
+            a = 1;
+            count = 200;
             // TARO とぶつかった時の処理を書く
         }  
     }
-        public void hitd()
+
+    public void hitd()
     {
         Actor actor = getOneObjectAtOffset( 0, 0, bulletd.class );
         if( actor != null ){
@@ -91,4 +95,29 @@ public class oni extends Actor
             // TARO とぶつかった時の処理を書く
         }  
     }
+    /*
+    public void hit()
+    {
+    Actor actor = getOneObjectAtOffset( 0, 0, tank.class );
+    if( actor != null ){
+    getWorld().removeObject( actor ); // getWorld() は World オブジェクトの getter
+    oni++;
+    getWorld().showText( "捕まえた数"+oni, 100, 50 );
+    if(oni == 10){
+    getWorld().showText( "Clear !", 100, 50 );
+    Greenfoot.stop();
+    }
+    // TARO とぶつかった時の処理を書く
+    if (a != 1){
+
+    World c = new gameover();
+    Greenfoot.setWorld(c);
+    Greenfoot.stop();
+
+    }
+    }  
+
+    }
+     */
+
 }
--- game/tank.java
+++ game/tank.java
@@ -17,120 +17,130 @@ public class tank extends Actor
     private int fs=0;
     private int fw=0;
     private int fd=0;
-    
+
     private boolean flag_tamaw = false;
     private boolean flag_tamaa = false;
     private boolean flag_tamas = false;
     private boolean flag_tamad = false;
-    
+
     public void act() 
     {
 
-    hit ();
-    int x = getX();
-    int y = getY();
+        hit ();
+        int x = getX();
+        int y = getY();
 
         if( Greenfoot.isKeyDown( "right" ) ){
-        setRotation(0);
-        move(4);
-        
-    }
-     if( Greenfoot.isKeyDown( "left" ) ){
-        setRotation(180);
-        move(4);
-    }
-     if( Greenfoot.isKeyDown( "down" ) ){
-        setRotation(90);
-        move(4);
-    }
-     if( Greenfoot.isKeyDown( "up" ) ){
-        setRotation(270);
-        move(4);
-    }
-    
-    if(fs == 0){/*
-    if( Greenfoot.isKeyDown( "s" ) ){
-        getWorld().addObject( new bullets(), x, y );
-        fs=1;
-    }
-    }
-    if(fa == 0){
-    if( Greenfoot.isKeyDown( "a" ) ){
-        getWorld().addObject( new bulleta(), x, y );
-        fa=1;
-    }
+            setRotation(0);
+            move(4);
+
+        }
+        if( Greenfoot.isKeyDown( "left" ) ){
+            setRotation(180);
+            move(4);
+        }
+        if( Greenfoot.isKeyDown( "down" ) ){
+            setRotation(90);
+            move(4);
+        }
+        if( Greenfoot.isKeyDown( "up" ) ){
+            setRotation(270);
+            move(4);
+        }
+
+        if(fs == 0){/*
+            if( Greenfoot.isKeyDown( "s" ) ){
+            getWorld().addObject( new bullets(), x, y );
+            fs=1;
+            }
+            }
+            if(fa == 0){
+            if( Greenfoot.isKeyDown( "a" ) ){
+            getWorld().addObject( new bulleta(), x, y );
+            fa=1;
+            }
+            }
+
+            if(fw == 0){
+            if( Greenfoot.isKeyDown( "w" ) ){
+            getWorld().addObject( new bulletw(), x, y );
+            fw=1;
+            }
+            }
+
+            if(fd == 0){
+            if( Greenfoot.isKeyDown( "d" ) ){
+            getWorld().addObject( new bulletd(), x, y );
+            fd=1;
+            }
+             */
+
+            if( Greenfoot.isKeyDown( "s" ) ){
+
+                if( flag_tamas == false ){
+                    getWorld().addObject( new bullets(), x, y );
+                    flag_tamas = true;
+                }
+            }  
+            else flag_tamas = false;
+
+            if( Greenfoot.isKeyDown( "a" ) ){
+
+                if( flag_tamaa == false ){
+                    getWorld().addObject( new bulleta(), x, y );
+                    flag_tamaa = true;
+                }
+            }  
+            else flag_tamaa = false;
+
+            if( Greenfoot.isKeyDown( "w" ) ){
+
+                if( flag_tamaw == false ){
+                    getWorld().addObject( new bulletw(), x, y );
+                    flag_tamaw = true;
+                }
+            }  
+            else flag_tamaw = false;
+
+            if( Greenfoot.isKeyDown( "d" ) ){
+
+                if( flag_tamad == false ){
+                    getWorld().addObject( new bulletd(), x, y );
+                    flag_tamad = true;
+                }
+            }  
+            else flag_tamad = false;
+
+        } 
     }
 
-    if(fw == 0){
-    if( Greenfoot.isKeyDown( "w" ) ){
-        getWorld().addObject( new bulletw(), x, y );
-        fw=1;
-    }
-    }
+    private void hit()
+    {
+        oni actor = (oni)getOneObjectAtOffset( 0, 0, oni.class );
+        if( actor != null ){
 
-    if(fd == 0){
-    if( Greenfoot.isKeyDown( "d" ) ){
-        getWorld().addObject( new bulletd(), x, y );
-        fd=1;
+            if ( actor.moving() ){
+
+                World c = new gameover();
+                Greenfoot.setWorld(c);
+                Greenfoot.stop();
+            }
+            else{
+
+                getWorld().removeObject( actor ); // getWorld() は World オブジェクトの getter
+                oni++;
+                getWorld().showText( "捕まえた数"+oni, 100, 50 );
+
+                if(oni == 10)
+                {
+                    getWorld().showText( "Clear !", 100, 50 );
+                    Greenfoot.stop();
+
+                }
+
+            }
+        }  
+
     }
-    */
-    
-     if( Greenfoot.isKeyDown( "s" ) ){
-            
-            if( flag_tamas == false ){
-                getWorld().addObject( new bullets(), x, y );
-                flag_tamas = true;
-            }
-        }  
-        else flag_tamas = false;
-    
-    if( Greenfoot.isKeyDown( "a" ) ){
-            
-            if( flag_tamaa == false ){
-                getWorld().addObject( new bulleta(), x, y );
-                flag_tamaa = true;
-            }
-        }  
-        else flag_tamaa = false;
-
-    if( Greenfoot.isKeyDown( "w" ) ){
-            
-            if( flag_tamaw == false ){
-                getWorld().addObject( new bulletw(), x, y );
-                flag_tamaw = true;
-            }
-        }  
-        else flag_tamaw = false;
-
-    if( Greenfoot.isKeyDown( "d" ) ){
-            
-            if( flag_tamad == false ){
-                getWorld().addObject( new bulletd(), x, y );
-                flag_tamad = true;
-            }
-        }  
-        else flag_tamad = false;
-        
-        
-    
-        
-    } 
 }
 
-    public void hit()
-    {
-        Actor actor = getOneObjectAtOffset( 0, 0, oni.class );
-        if( actor != null ){
-            getWorld().removeObject( actor ); // getWorld() は World オブジェクトの getter
-            oni++;
-            getWorld().showText( "捕まえた数"+oni, 100, 50 );
-            if(oni == 10){
-                   getWorld().showText( "Clear !", 100, 50 );
-                   Greenfoot.stop();
-                }
-            // TARO とぶつかった時の処理を書く
-        }  
-    }
-    
-
-    }    
